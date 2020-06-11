d408bc174b78b294b98bd197f153772ae90f64f3
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-10 22:04:57 -0700

    being in react router, setup Navbar and Links

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i> {title}
+      </h1>
+      <ul>
+        <li>
+          <NavLink exact activeStyle={{ textDecoration: "underline" }} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            exact
+            activeStyle={{ textDecoration: "underline" }}
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Contact Manager",
+  icon: "fas fa-id-card-alt",
+};
+
+export default Navbar;
